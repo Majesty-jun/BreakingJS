923003380274123a158ff2ba79750c84c201edab
Majesty-jun <hkwns01@gmail.com>
2022-12-27 21:18:10 +0900

    Feat: Add checking AM/PM function

--- clock.js
+++ clock.js
@@ -1,11 +1,29 @@
 const clock = document.querySelector(".clock");
 
+const time = {
+  hours: 0,
+  minutes: 0,
+  seconds: 0
+};
+
 function getClock() {
   const now = new Date();
-  const hours = String(now.getHours()).padStart(2, '0');;
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  const seconds = String(now.getSeconds()).padStart(2, '0');
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+  let AMPM = 'AM';
+  time.hours = now.getHours();
+  time.minutes = String(now.getMinutes()).padStart(2, '0');
+  time.seconds = String(now.getSeconds()).padStart(2, '0');
+  AMPM = dayOrNight(time);
+  time.hours = String(time.hours).padStart(2, '0');
+  clock.innerText = `${time.hours}:${time.minutes}:${time.seconds} ${AMPM}`;
+};
+
+function dayOrNight(time) {
+  let AMPM = 'AM';
+  if (time.hours > 12) {
+    time.hours -= 12;
+    AMPM = 'PM';
+  }
+  return AMPM;
 }
 
 getClock();
