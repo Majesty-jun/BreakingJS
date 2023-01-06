a3a7d3d4bba084efb6b471d68156db5cc4a7d019
Majesty-jun <hkwns01@gmail.com>
2023-01-07 01:17:20 +0900

    Feat: Show Box with Img & Number

---
+++ lightbox.js
@@ -0,0 +1,15 @@
+const img = document.querySelectorAll('.itemImg');
+const container = document.querySelector('.container');
+const lightBox = document.querySelector('.lightBox');
+const lightImg = document.querySelector('.lightImg');
+const imgNumber = document.querySelector('.imgNumber')
+const close = document.querySelector('.close');
+const [prevBtn, nextBtn, closeBtn] = document.querySelectorAll("button");
+
+img.forEach((element, index) => {
+  element.addEventListener('click', (e) => {
+    lightBox.style.display = "block";
+    lightImg.src = img[index].src;
+    imgNumber.innerText = `Index ${index + 1} of ${img.length}`;
+  })
+})
