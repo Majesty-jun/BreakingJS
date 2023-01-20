68c5cc52fa2b3d57fe7eaabd5d6e9266dbd3f08b
Majesty-jun <hkwns01@gmail.com>
2023-01-21 01:27:21 +0900

    Feat: SearchInputHandler function of search bar

---
+++ search.js
@@ -0,0 +1,50 @@
+let data = [
+  'searchBarClickHandler',
+  'getCryptoKey',
+  'Uint32Array',
+  'getRandomValues',
+  'window',
+  'target',
+  'searchBar',
+  'getAttribute',
+  'autoComplete',
+  'border',
+  'searchSubmitHandler',
+  'preventDefault',
+  'getCryptoKeys',
+  'localStorage',
+  'getAutoCompleteList',
+  '일어나',
+]
+
+
+const searchButton = document.querySelector('.searchButton');
+const searchBar = document.querySelector('.searchbar');
+const searchForm = document.querySelector('.searchForm');
+const suggestion = document.querySelector('.suggestion');
+
+let searchInput = '';
+
+function styleBorderRadius(node, size) {
+  node.style.borderRadius = `${size}`;
+}
+
+function renderSuggestion() {
+  suggestion.innerHTML = '';
+  data.forEach(element => {
+    if (searchInput === '') return;
+    if (element.startsWith(searchInput)) {
+      styleBorderRadius(searchBar, '5px 5px 0 0');
+      const template = /* html */`<div class='autosuggestion'>${element}</div>`
+      suggestion.insertAdjacentHTML('beforeend', template);
+    }
+  })
+}
+
+function searchInputHandler(e) {
+  searchInput = e.target.value;
+  styleBorderRadius(searchBar, '5px');
+  renderSuggestion();
+}
+
+searchBar.addEventListener('input', searchInputHandler); 
